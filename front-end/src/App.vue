0215d65913b4a63bfbd6e525345c2b2fc7d8d5b6
Xinghua SHAO <xinghua.shao@utc.etu.fr>
2023-01-04 20:08:01 +0100

    update

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -104,18 +104,18 @@
               <div class="button desactiverAccueil" @click="desactiverAccueil"  style="float: left; margin-top: auto; margin-bottom: auto">➖</div>
             </div>
             <div style="width:33.3%; margin-left: 0;justify-content:center">
-              <div class="Accueil nbMedecin" style=" margin-right: auto; margin-left: auto;background: #ffa64d;height: 14rem; width:100%">
+              <div class="nbMedecin" style=" margin-right: auto; margin-left: auto;background: #ffa64d;height: 14rem; width:100%">
                 Nombre de medecin niveau 1:{{items[4][0]}}<br>
                 Nombre de medecin niveau 2:{{items[4][1]}}<br>
                 Nombre de medecin niveau 3:{{items[4][2]}}<br>
                 Nombre de medecin niveau 4:{{items[4][3]}}<br>
                 Nombre de medecin niveua 5:{{items[4][4]}}<br>
                 
-                  <div class="input box" style="margin-left:13rem">
-                    <input v-model="levelMedecin" type="number" style="width:5rem; height:2rem; float:right" min=1 max=5>
+                  <div class="input box" style="">
+                    <input v-model="levelMedecin" type="number" style="width:5rem; height:2rem; float:left" min=1 max=5>
                   </div>
-                  <div class="button activerDoc" @click="activerDoc" style="float: right" >➕</div>
-                  <div class="button desactiverDoc" @click="desactiverDoc" style="float: right">➖</div>
+                  <div class="button activerDoc" @click="activerDoc" style="float: left" >➕</div>
+                  <div class="button desactiverDoc" @click="desactiverDoc" style="float: left">➖</div>
                 
               
           </div>
@@ -733,7 +733,6 @@ main .Room {
   border-radius: 0.8rem;
   border: 0.3rem solid white;
   padding-bottom: 1.5rem;
-
 }
 
 /*Espaces pour renseigner les infos du noveau patient*/
